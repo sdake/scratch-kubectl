7372ff4e7092961bc7d4e6a5a09ce552e50fbefe
Steven Dake <stdake@cisco.com>
2018-09-22 09:53:16 -0700

    Don't dirty up the environment with work
    The old build.sh script would build in the CWD.  This was a small
    problem as a bunch of files were left over after a build.  Instead
    build in secure /tmp directories (in a ramdisk) instead.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -x
 
 ROOT=$(mktemp -d)
+BUILD=$(mktemp -d)
+DOCKER_CTX=$(mktemp -d)
 
 BASH_VERSION="4.4"
 COREUTILS_VERSION="8.30"
@@ -10,18 +12,21 @@ GAWK_VERSION="4.2.1"
 GREP_VERSION="3.1"
 KUBECTL_VERSION="v1.10.8"
 
-BASH_URL=https://ftp.gnu.org/gnu/bash/bash-${BASH_VERSION}.tar.gz
-COREUTILS_URL=https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VERSION}.tar.xz
-GAWK_URL=https://ftp.gnu.org/gnu/gawk/gawk-${GAWK_VERSION}.tar.xz
-GREP_URL=https://ftp.gnu.org/gnu/grep/grep-${GREP_VERSION}.tar.xz
-KUBECTL_URL=https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+BASH_URL="https://ftp.gnu.org/gnu/bash/bash-${BASH_VERSION}.tar.gz"
+COREUTILS_URL="https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VERSION}.tar.xz"
+GAWK_URL="https://ftp.gnu.org/gnu/gawk/gawk-${GAWK_VERSION}.tar.xz"
+GREP_URL="https://ftp.gnu.org/gnu/grep/grep-${GREP_VERSION}.tar.xz"
+KUBECTL_URL="https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+
+cp Dockerfile ${DOCKER_CTX}
+
+pushd ${BUILD}
 
 curl -LO ${BASH_URL}
 curl -LO ${COREUTILS_URL}
 curl -LO ${GAWK_URL}
 curl -LO ${GREP_URL}
 curl -LO ${KUBECTL_URL}
-chmod +x kubectl
 
 # TODO(sdake) Validate signatures
 
@@ -52,14 +57,17 @@ popd
 
 strip -s kubectl
 cp -a kubectl ${ROOT}/bin
+install -s -m=755 kubectl ${ROOT}/bin
 
-pushd ${ROOT}
 # remove build path context from shebange binaries for coreutils
+pushd ${ROOT}
 pushd bin
 find . -size 1k -exec sed -i -e "s#$ROOT##g" {} \;
 popd
 
-tar -czf /home/sdake/scratch-kubectl/root.tar.gz bin
-popd
+tar -czf ${DOCKER_CTX}/root.tar.gz bin
+popd #${ROOT}
+popd #${BUILD)
 
-docker build -t sdake/kubectl .
+docker build -t sdake/kubectl ${DOCKER_CTX}
+echo "Done building static kubectl container"
